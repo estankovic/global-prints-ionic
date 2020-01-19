1884a364daff07a7084b0cd7a7856db5bcc46e96
Eduard Stankovic <eduard.stankovic@gmail.com>
2020-01-19 16:15:40 +0100

    code moved 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,64 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { MatSidenavModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { EffectsModule } from '@ngrx/effects';
+import { MinimalRouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { metaReducers, reducers } from './metaReducers';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyA2OUra2-wclZY-du9NNnmgP0wAiQpeWHY',
+      authDomain: 'global-prints-pwa.firebaseapp.com',
+      databaseURL: 'https://global-prints-pwa.firebaseio.com',
+      projectId: 'global-prints-pwa',
+      storageBucket: 'global-prints-pwa.appspot.com',
+      messagingSenderId: '204453083183',
+      appId: '1:204453083183:web:b967350c8a3d803078aeda',
+    }),
+    AngularFirestoreModule.enablePersistence(),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 100,
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      serializer: MinimalRouterStateSerializer,
+    }),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    SharedModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
